4c08bc51aae5380fce92896b7d7f559b170cb9c8
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 11:49:10 -0300

    added first setup for EF

---
+++ VirtualMindTest/DataAccess/VirtualMindContext.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class VirtualMindContext : DbContext
+    {
+        public VirtualMindContext() : base("VirtualMindContext") { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ VirtualMindTest/DataAccess/VirtualMindInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class VirtualMindInitializer : DropCreateDatabaseIfModelChanges<VirtualMindContext>
+    {
+        protected override void Seed(VirtualMindContext context)
+        {
+            var users = new List<User>
+            {
+                new User { FirstName = "Pablo", LastName="Mendez", Email="mendez.developer@gmail.com", Password="1234" },
+                new User { FirstName = "Virtual", LastName="Mind", Email="admin@gvirtualmind.com", Password="1234" }
+            };
+
+            users.ForEach(u => context.Users.Add(u));
+            context.SaveChanges();
+        }
+    }
+}
