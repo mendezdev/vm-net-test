f3494c51b2080b6f9ea9d858f1d9cae267a84ade
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 19:30:13 -0300

    added quotation test

---
+++ VirtualMindTest/Tests/Quotation/QuotationTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Domain.Impl.Client;
+using Domain;
+
+namespace Tests.Quotation
+{
+    [TestClass]
+    public class QuotationTest
+    {
+        private Mock<IQuotationDomain> quotationDomain;
+        private Mock<IApiClient> apiClient;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            quotationDomain = new Mock<IQuotationDomain>();
+            apiClient = new Mock<IApiClient>();
+        }
+
+        //[TestMethod]
+        //[TestCategory("Quotation")]
+        //public async Task GetQuotationInformationOk()
+        //{
+        //    apiClient.Setup(a => a.GetAsync(It.IsAny<string>()))
+        //        .ReturnsAsync()
+        //}
+    }
+}
