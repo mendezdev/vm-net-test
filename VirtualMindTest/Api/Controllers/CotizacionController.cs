0d9ae6d6b12bb66b34a2253fcc38b3dde4c16dee
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 11:02:46 -0300

    added logic to call api

--- VirtualMindTest/Api/Controllers/CotizacionController.cs
+++ VirtualMindTest/Api/Controllers/CotizacionController.cs
@@ -1,18 +1,35 @@
-﻿using System;
+﻿using Domain;
+using Domain.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Api.Controllers
 {
     public class CotizacionController : ApiController
     {
-        [HttpGet]
-        public IHttpActionResult Get()
+        private readonly IQuotationDomain quotationDomain;
+
+        public CotizacionController()
         {
-            return Ok(new { message = "Success!" });
+            quotationDomain = new QuotationDomain();
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(string id)
+        {
+            var response = await quotationDomain.GetQuotation(id);
+
+            if (response.HasError)
+            {
+                return Content((HttpStatusCode)response.CodeError, response.Message);
+            }
+
+            return Ok(response.ObjectResponse);
         }
     }
 }
--- VirtualMindTest/Domain.Impl/QuotationDomain.cs
+++ VirtualMindTest/Domain.Impl/QuotationDomain.cs
@@ -8,27 +8,39 @@ using ViewModels;
 using Domain.Impl.Constants;
 using Domain.Impl.Formatters;
 using System.Web.Configuration;
+using System.Net.Http;
 
 namespace Domain.Impl
 {
     public class QuotationDomain : IQuotationDomain
     {
         private readonly IDictionary<string, Func<Task<Response<QuotationResponse>>>> cotizationStrategy;
+        private readonly HttpClient client;
 
         public QuotationDomain()
         {
+            client = new HttpClient();
             cotizationStrategy = new Dictionary<string, Func<Task<Response<QuotationResponse>>>>();
-            //cotizationStrategy.Add(CurrencyType.DOLAR, GetDolarQuotation);
+            cotizationStrategy.Add(CurrencyType.DOLAR, GetDolarQuotation);
             cotizationStrategy.Add(CurrencyType.PESOS, GetRealQuotation);
             cotizationStrategy.Add(CurrencyType.REAL, GetPesosQuotation);
         }
 
         public async Task<Response<QuotationResponse>> GetQuotation(string currency)
         {
-            return new Response<QuotationResponse>
+            try
             {
-                ObjectResponse = new QuotationResponse { PurchasePrice = 10, SalePrice = 20 }
-            };
+                return await cotizationStrategy[currency]();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return new Response<QuotationResponse>
+                {
+                    CodeError = 500,
+                    HasError = true,
+                    Message = "El tipo de moneda no esta contemplado."
+                };
+            }
         }
 
         private Task<Response<QuotationResponse>> GetRealQuotation()
@@ -41,25 +53,25 @@ namespace Domain.Impl
             return GetUnhautorizedError();
         }
 
-        //private async Task<Response<QuotationResponse>> GetDolarQuotation()
-        //{
-        //    var response = new Response<QuotationResponse>();
-        //    var formatter = new QuotationFormatter();
+        private async Task<Response<QuotationResponse>> GetDolarQuotation()
+        {
+            var response = new Response<QuotationResponse>();
+            var formatter = new QuotationFormatter();
 
-        //    var uri = WebConfigurationManager.AppSettings["QuotationUrl"];
-        //    var result = await client.GetAsync(uri);
-        //    if (!result.IsSuccessStatusCode)
-        //    {
-        //        response.HasError = true;
-        //        response.Message = "Ocurrió un problema al intentar obtener la cotización. Intente de nuevo mas tarde, por favor.";
-        //        response.CodeError = 401;
-        //        return response;
-        //    }
+            var uri = WebConfigurationManager.AppSettings["QuotationUrl"];
+            var result = await client.GetAsync(uri);
+            if (!result.IsSuccessStatusCode)
+            {
+                response.HasError = true;
+                response.Message = "Ocurrió un problema al intentar obtener la cotización. Intente de nuevo mas tarde, por favor.";
+                response.CodeError = 401;
+                return response;
+            }
 
-        //    var quotation = await result.Content.ReadAsAsync<List<string>>();
-        //    response.ObjectResponse = formatter.ToQuotationResponse(quotation);
-        //    return response;
-        //}
+            var quotation = await result.Content.ReadAsAsync<List<string>>();
+            response.ObjectResponse = formatter.ToQuotationResponse(quotation);
+            return response;
+        }
 
         private Task<Response<QuotationResponse>> GetUnhautorizedError()
         {
