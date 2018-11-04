aff8fd3b201c84ec4874b0d78752d8cd73096aa3
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 19:15:19 -0300

    added httpclient wrapper

---
+++ VirtualMindTest/Domain.Impl/Client/IApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Impl.Client
+{
+    public interface IApiClient
+    {
+        Task<HttpResponseMessage> GetAsync(string uri);
+    }
+}
