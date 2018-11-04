f0e023720ee78cbc689cf219a63bdfe0506e046f
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 10:49:32 -0300

    added view models

---
+++ VirtualMindTest/ViewModels/QuotationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class QuotationResponse
+    {
+        public decimal SalePrice { get; set; }
+        public decimal PurchasePrice { get; set; }
+    }
+}
---
+++ VirtualMindTest/ViewModels/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class Response<T>
+    {
+        public T ObjectResponse { get; set; }
+        public bool HasError { get; set; }
+        public int CodeError { get; set; }
+        public string Message { get; set; }
+    }
+}
