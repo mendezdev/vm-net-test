d47bb0fa2418cc6955da9861917e6223ea944cbf
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 10:06:53 -0300

    added cotizacion controller and changed uri

--- VirtualMindTest/Api/App_Start/WebApiConfig.cs
+++ VirtualMindTest/Api/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "virtualmind/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ VirtualMindTest/Api/Controllers/CotizacionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class CotizacionController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(new { message = "Success!" });
+        }
+    }
+}
