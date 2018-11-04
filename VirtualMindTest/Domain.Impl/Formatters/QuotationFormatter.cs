98ebfd0c7a924b0d2fd6ceddcbdc4e84d7b42f31
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 10:49:41 -0300

    added formatter

---
+++ VirtualMindTest/Domain.Impl/Formatters/QuotationFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace Domain.Impl.Formatters
+{
+    public class QuotationFormatter
+    {
+        public QuotationResponse ToQuotationResponse(IList<string> quotation)
+        {
+            // 0 -> purchase, 1 -> sale, 2 -> last update
+            var purchasePrice = Convert.ToDecimal(quotation[0]);
+            var salePrice = Convert.ToDecimal(quotation[1]);
+            return new QuotationResponse
+            {
+                SalePrice = salePrice,
+                PurchasePrice = purchasePrice
+            };
+        }
+    }
+}
