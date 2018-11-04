85d39550b51e2f43b9fbe7466802eb3fc57e66e2
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 15:24:58 -0300

    added user formatter

---
+++ VirtualMindTest/Domain.Impl/Formatters/UserFormatter.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace Domain.Impl.Formatters
+{
+    public class UserFormatter
+    {
+        public UserResponse ToUserResponse(User user)
+        {
+            return new UserResponse
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+        }
+    }
+}
