b3176c2a220c4a1ca2b611e7113e76cd2483a5da
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 10:50:00 -0300

    added quotation type constant

---
+++ VirtualMindTest/Domain.Impl/Constants/CurrencyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Impl.Constants
+{
+    public class CurrencyType
+    {
+        public const string PESOS = "Pesos";
+        public const string DOLAR = "Dolar";
+        public const string REAL = "Real";
+    }
+}
