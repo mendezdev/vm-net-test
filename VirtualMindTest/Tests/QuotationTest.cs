0d5b6ce1d8d89faab08d4b67cae868be99222237
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 18:55:56 -0300

    improved configuration tests

--- VirtualMindTest/Tests/QuotationTest.cs
+++ VirtualMindTest/Tests/QuotationTest.cs
@@ -1,20 +1,40 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Domain;
+using Domain.Impl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModels;
 
 namespace Tests
 {
     [TestClass]
     public class QuotationTest
     {
+        private Mock<IQuotationDomain> quotationDomainMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            quotationDomainMock = new Mock<IQuotationDomain>();
+        }
+
         [TestMethod]
         [TestCategory("Quotation")]
-        public void GetQuotationInformationOk()
+        public async Task GetQuotationInformationOk()
         {
-            Assert.AreEqual(1, 1);
+            quotationDomainMock.Setup(q => q.GetQuotation("Dolar"))
+                .ReturnsAsync(new QuotationResponse { PurchasePrice = 100, SalePrice = 100 });
+            var quotationDomain = new QuotationDomain();
+
+            var result = await quotationDomain.GetQuotation("Dolar");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.PurchasePrice, 100);
+            Assert.AreEqual(result.SalePrice, 100);
         }
     }
 }
