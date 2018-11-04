0f1fe8c699baf4fc7ca903b6eafbc4f8cff9cd28
Pablo Mendez <mendez.developer@gmail.com>
2018-11-04 18:35:57 -0300

    added test project

---
+++ VirtualMindTest/Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Test")]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Test")]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("Pablo", "Pablo");
+        }
+    }
+}
